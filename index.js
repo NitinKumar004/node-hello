ba3abc92571add8f864e001d1427298b28e9c29f
naveen-bandela-au2 <b.naveen2085@gmail.com>
2026-04-28 13:02:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'hai adfdfdf !\n'
+  const msg = 'hai adfdfd !\n'
   res.end(msg);
 });
 
