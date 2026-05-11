a62ed1f8167d281cfce483138b2975d94ff9700e
naveen-bandela-au2 <b.naveen2085@gmail.com>
2026-05-11 17:58:17 +0530

    Update greeting message in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'hai zopday1 !\n'
+  const msg = 'hai zopday2 !\n'
   res.end(msg);
 });
 
